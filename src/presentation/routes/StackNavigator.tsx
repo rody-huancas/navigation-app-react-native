f10497fd281a306ac5a7fba6d42aa02a97739e9a
Rody Huancas <rodyhuancas.04@gmail.com>
2025-01-05 00:33:52 -0500

    Feat: flatlist

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,6 +1,7 @@
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {HomeScreen} from '../screens/home/HomeScreen';
+import { ProductScreen } from '../screens/products/ProductScreen';
 import { ProductsScreen } from '../screens/products/ProductsScreen';
 import { SettingsScreen } from '../screens/settings/SettingsScreen';
 
@@ -8,9 +9,16 @@ const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{
+      headerShown: true,
+      headerStyle: {
+        elevation: 0,
+        shadowColor: 'transparent',
+      }
+    }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
--- src/presentation/screens/products/ProductsScreen.tsx
+++ src/presentation/screens/products/ProductsScreen.tsx
@@ -1,9 +1,43 @@
 import {Text, View} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
+
+import {globalStyles} from '../../theme/theme';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+
+const products = [
+  {id: 1, name: 'Product 1'},
+  {id: 2, name: 'Product 2'},
+  {id: 3, name: 'Product 3'},
+  {id: 4, name: 'Product 4'},
+  {id: 5, name: 'Product 5'},
+  {id: 6, name: 'Product 6'},
+];
 
 export const ProductsScreen = () => {
+  const {navigate} = useNavigation();
+
   return (
-    <View>
-      <Text>ProductsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{marginBottom: 10, fontSize: 30}}>Productos</Text>
+
+      <FlatList
+        data={products}
+        renderItem={({item}) => (
+          <PrimaryButton
+            onPress={() => navigate('Product' as never)}
+            label={item.name}
+          />
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
+
+      <Text style={{marginBottom: 10, fontSize: 30}}>Ajustes</Text>
+
+      <PrimaryButton
+        label="Ajustes"
+        onPress={() => navigate('Settings' as never)}
+      />
     </View>
   );
 };
